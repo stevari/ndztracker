f1033b213dbb7e228bbe5836932c224221afedd6
Ville Vartia <ville.v.vartia@utu.fi>
2022-12-28 15:23:40 +0200

    front-end can now retrieve and show drone data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,36 @@
 import React from 'react'
+import { useState,useEffect } from 'react'
+
+
 
 export default function App() {
+  const [backendData,setBackendData] = useState([{}])
+  //NOTETOSELF remember to start server before launch
+  useEffect(() => {
+    fetch("/drones")
+    .then(
+      response => response.json() 
+    )
+    .then(
+      data => {
+        setBackendData(data)
+      }
+    )
+  },[])
+
   return (
-    <div>App</div>
+    <div>
+      {(typeof backendData.drones ==='undefined') ? (
+        <p>loading...</p>
+      ):(
+        backendData.drones.map(drone => (
+          <div key={drone.serialNumber}>
+            <p>{`drone serial number: ${drone.serialNumber} 
+                positionY: ${drone.positionY}
+                positionX: ${drone.positionX}`}</p>
+          </div>
+        ))
+      )}
+    </div>
   )
 }
