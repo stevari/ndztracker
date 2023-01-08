4f2613232386e070426a56d3ebe035b27c05f6de
Ville Vartia <ville.v.vartia@utu.fi>
2023-01-08 13:31:05 +0200

    removed (now) useless fetches

--- frontend/src/components/Body.js
+++ frontend/src/components/Body.js
@@ -1,15 +1,7 @@
 import React from 'react'
-import { useEffect } from 'react'
 import DisplayPilots from "./DisplayPilots"
-//get and display only pilot data
 export default function Body
 () {
-  useEffect(() => {
-    fetch("api/drones")
-    .then(
-      fetch("api/violatingdrones")
-    )
-  },[])
   return (
     <div style={{height:"100vh"}}>
         <DisplayPilots/>
